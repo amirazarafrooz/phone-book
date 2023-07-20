300935640e6db31e7a9934d0433ad27482c2e4f5
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-07-20 18:28:17 +0330

    fix: test image for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import Home from "./pages/Home";
 import NewContact from "./pages/NewContact";
 import { ContactDetail } from "./pages/ContactDetail-page";
 import EditContact from "./pages/EditContact-page";
-import "../src/css/app.css"
-import "../src/css/button.css"
-import "../src/css/home.css"
-import "../src/css/contact.css"
-import "../src/css/contactlist.css"
-import "../src/css/newcontact.css"
-import "../src/css/contactform.css"
-import "../src/css/contactdetail.css"
-import "../src/css/contactdetail-comp.css"
-import "../src/css/editcontact.css"
-import "../src/css/editcontact-comp.css"
+import "../src/css/app.css";
+import "../src/css/button.css";
+import "../src/css/home.css";
+import "../src/css/contact.css";
+import "../src/css/contactlist.css";
+import "../src/css/newcontact.css";
+import "../src/css/contactform.css";
+import "../src/css/contactdetail.css";
+import "../src/css/contactdetail-comp.css";
+import "../src/css/editcontact.css";
+import "../src/css/editcontact-comp.css";
+
+
 
 
 
--- src/pages/ContactDetail-page.jsx
+++ src/pages/ContactDetail-page.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import {Img} from 'react-image'
+import backicon from "../images/back-icon.png"
+
+
+
 
 
 import { Contactdetail } from "../components/Contactdetail";
@@ -7,7 +12,10 @@ export const ContactDetail = () => {
   
 
   return (
+
+    
     <div className="container">
+      <Img src={backicon}></Img>
       <div className="contactdetail__container">
         <div className="contactdetail__header">
       <h1> Contact info </h1>
